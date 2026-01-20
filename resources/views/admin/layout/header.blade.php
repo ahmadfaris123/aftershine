1750cbb10ecfb1c72c71f6d98e0f7c0b6600d637
ahmadfaris123 <ahmadnur.faris22@gmail.com>
2026-01-20 16:29:57 +0700

    first commit

---
+++ resources/views/admin/layout/header.blade.php
@@ -0,0 +1,39 @@
+<main class="dashboard-main">
+  <div class="navbar-header">
+  <div class="row align-items-center justify-content-between">
+    <div class="col-auto">
+      <div class="d-flex flex-wrap align-items-center gap-4">
+        <button type="button" class="sidebar-toggle">
+          <iconify-icon icon="heroicons:bars-3-solid" class="icon text-2xl non-active"></iconify-icon>
+          <iconify-icon icon="iconoir:arrow-right" class="icon text-2xl active"></iconify-icon>
+        </button>
+        <button type="button" class="sidebar-mobile-toggle">
+          <iconify-icon icon="heroicons:bars-3-solid" class="icon"></iconify-icon>
+        </button>
+      </div>
+    </div>
+    <div class="col-auto">
+      <div class="d-flex flex-wrap align-items-center gap-3">
+        <button type="button" data-theme-toggle
+          class="w-40-px h-40-px bg-neutral-200 rounded-circle d-flex justify-content-center align-items-center">
+        </button>
+
+        <div class="dropdown">
+          <button class="d-flex justify-content-center align-items-center rounded-circle" type="button"
+            data-bs-toggle="dropdown">
+            <img src="assets/images/user.png" alt="image" class="w-40-px h-40-px object-fit-cover rounded-circle">
+          </button>
+          <div class="dropdown-menu to-top dropdown-menu-sm">
+            <ul class="to-top-list">
+              <li>
+                <a class="dropdown-item text-black px-0 py-8 hover-bg-transparent hover-text-danger d-flex align-items-center gap-3"
+                  href="javascript:void(0)">
+                  <iconify-icon icon="lucide:power" class="icon text-xl"></iconify-icon> Log Out</a>
+              </li>
+            </ul>
+          </div>
+        </div><!-- Profile dropdown end -->
+      </div>
+    </div>
+  </div>
+</div>

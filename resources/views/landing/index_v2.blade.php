898d0dc82bf5aadd54e2127853fcef93dcc0563d
ahmadfaris123 <ahmadnur.faris22@gmail.com>
2026-05-23 13:55:03 +0700

    theme

--- resources/views/landing/index_v2.blade.php
+++ resources/views/landing/index_v2.blade.php
@@ -773,7 +773,7 @@
         </div>
         <div class="spotify-body collapsed" id="spotifyBody">
             <!-- Ganti src iframe dibawah dengan link embed spotify yang diinginkan -->
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6daEl3JyMDgK52fKuqPelL?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6daEl3JyMDgK52fKuqPelL?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div>
     </div>
 
